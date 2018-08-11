50ba220f8874e008a84560daa754eab4bd883029
Craig Wilson <craig@wilsoncg.net>
2018-08-11 21:32:35 +0100

    write some xml

--- Start.ps1
+++ Start.ps1
@@ -1,4 +1,4 @@
-$client1 = [PSCustomObject]@{
+$customer = [PSCustomObject]@{
 	ClientAccountId = 1
 	ClientCode = 'clientCode'
 	ContractId = 10
@@ -26,16 +26,68 @@ $client1 = [PSCustomObject]@{
 		}
 	)
 }
-$client1
+
+# c# can tell us assembly name
+# Console.WriteLine(typeof(System.Xml.XmlDocument).Assembly.FullName);
 
 $path = Join-Path (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent) "bpm_xsd.dll"
-$path
-Add-Type -AssemblyName $path -PassThru
+Add-Type -AssemblyName "System.Xml, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"
+# Add-Type -Namespace "bpm" -TypeName "BPMNDiagram" -AssemblyName $path -PassThru
+Add-Type -path $path
 
 # absolute path needed :(
 # [Reflection.Assembly]::LoadFile(".\bpm_xsd.dll")
 
-$xml = New-Object 
-Get-Member -InputObject $xsd
 
-#function ToBpmParticipant()
+function ToCustomer($customer)
+{
+	$participant = New-Object -TypeName bpm.tParticipant -Property (@{
+		'id'='Participant_customer_'+$customer.ClientCode;
+		'name'='Customer_'+$customer.ClientCode;
+		'processRef'='Process_'+$customer.ClientCode;
+	})
+
+	$collab = New-Object -TypeName bpm.tCollaboration -Property (@{
+		'id'='Collaboration_customer_'+ $customer.ClientCode;
+		'participant'= @($participant)
+	})
+	return $collab
+}
+
+function Serialize($definitions)
+{
+	$serializer = New-Object -typeName System.Xml.Serialization.XmlSerializer($definitions.GetType(), "http://bpmn.io/schema/bpmn")
+    $ms = New-Object -typeName System.IO.MemoryStream
+	$writerSettings = New-Object -TypeName System.Xml.XmlWriterSettings -Property (@{
+		'Encoding'= [System.Text.Encoding]::UTF8;
+		'CloseOutput'= $true;
+		'Indent' = $true;
+	})
+    $writer = [System.Xml.XmlWriter]::Create($ms, $writerSettings)
+                
+	Write-Verbose "Creating output.xml file..."
+    $ns = new-object -typename System.Xml.Serialization.XmlSerializerNamespaces
+    $ns.Add("","");
+    $ns.Add("bpmn", "http://www.omg.org/spec/BPMN/20100524/MODEL");
+	$ns.Add("bpmni", "http://www.omg.org/spec/BPMN/20100524/DI");
+	$ns.Add("dc", "http://www.omg.org/spec/DD/20100524/DC");
+	$ns.Add("di", "http://www.omg.org/spec/DD/20100524/DI");
+    $ns.Add("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+    
+	$serializer.Serialize($writer, $definitions, $ns)
+    
+	$ms.Position = 0
+	$streamReader = New-Object -TypeName System.IO.StreamReader($ms)
+	$data = $streamReader.ReadToEnd()
+
+	$ms.Close
+	return $data
+}
+
+$xml = New-Object -TypeName bpm.tDefinitions -Property (@{
+	'Items'= @(
+		ToCustomer($customer)
+	)
+})
+
+Write-Output (Serialize $xml)
--- xsd/generate.ps1
+++ xsd/generate.ps1
@@ -1,3 +1,3 @@
 # wget https://github.com/bpmn-io/bpmn-moddle
-# .\Vs2017.ps1
-xsd.exe .\BPMN20.xsd .\BPMNDI.xsd .\DC.xsd .\DI.xsd .\Semantic.xsd /classes /namespace:bpm
+# & (join-path ($profile | split-path) "vs2017.ps1")
+xsd.exe .\BPMN20.xsd .\BPMNDI.xsd .\DC.xsd .\DI.xsd .\Semantic.xsd /classes /namespace:bpm /out:..\bpm-xsd\
